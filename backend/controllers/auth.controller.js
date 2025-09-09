6eb4ba315012881b82de0cc63d199b6110d3548f
salihbezai <salihbezai98@gmail.com>
2025-09-09 10:18:35 +0100

    setting upe the signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,19 @@
+import User from "../model/user.model.js"
+
 export const signup =async (req,res)=>{
-    res.send("Sign up route called")
+    const {email,password,name} = req.body
+    try {
+        const userExists = await User.findOne({email})
+
+    if(userExists){
+        return res.status(400).json({message:"User already exists"})
+    }
+    const user = await User.create({name,email,password})
+
+    res.status(201).json({ user,message:"User created successfully"})
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
 }
 
 
--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -38,8 +38,9 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 })
 
-mongoose.model("User",userSchema)
 
+
+// Pre-save hook to hash password before saving to database
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next();
 
@@ -50,8 +51,11 @@ userSchema.pre("save",async function(next){
     }catch(error){
         next(error)
     }
-
 })
 
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+const User = mongoose.model("User",userSchema)
 
 export default User;
