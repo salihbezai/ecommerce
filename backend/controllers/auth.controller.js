385a38434d8a04ed14eea635e8087c5df1898df8
salihbezai <salihbezai98@gmail.com>
2025-09-09 12:55:41 +0100

    setting up the authRoute

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,36 @@
+import { redis } from "../lib/redis.js"
 import User from "../model/user.model.js"
+import jwt from "jsonwebtoken"
+
+const generateTokens = (userId) => {
+ const accessToken = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{
+    expiresIn:"15m"
+})
+
+const refreshToken = jwt.sign({userId},process.env.REFRESH_TOKEN_SECRET,{
+    expiresIn:"7d"
+})
+return {accessToken,refreshToken}
+}
+
+const storeRefreshToken = async(userId,refreshToken)=>{
+    await redis.set(`refresh_token:${userId}`,refreshToken,"EX",7*24*60*60)
+}
+
+const setCookies = (res,accessToken,refreshToken)=>{
+    res.cookie("accessToken",accessToken,{
+        httpOnly:true,
+        maxAge:15*60*1000, // 15 minutes
+        secure:process.env.NODE_ENV === "production",
+        sameSite:"strict" // prevents CSRF attack, cross-site request forgery
+    })
+    res.cookie("refreshToken",refreshToken,{
+        httpOnly:true,
+        maxAge:7*24*60*60*1000, // 7 days
+        secure:process.env.NODE_ENV === "production",
+        sameSite:"strict" // prevents CSRF attack, cross-site request forgery
+    })
+}
 
 export const signup =async (req,res)=>{
     const {email,password,name} = req.body
@@ -10,7 +42,19 @@ export const signup =async (req,res)=>{
     }
     const user = await User.create({name,email,password})
 
-    res.status(201).json({ user,message:"User created successfully"})
+    // authenticate
+    const { accessToken, refreshToken } = await generateTokens(user._id)
+    await storeRefreshToken(user._id,refreshToken)
+
+    setCookies(res,accessToken,refreshToken)
+
+    res.status(201).json({ user:{
+        _id:user._id,
+        name:user.name,
+        email:user.email,
+        role:user.role
+    },message:"User created successfully"})
+
     } catch (error) {
         res.status(500).json({message:error.message})
     }
