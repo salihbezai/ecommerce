2aa78802bb53700dc09b84b37916cc0bd435f375
salihbezai <salihbezai98@gmail.com>
2025-09-13 21:52:52 +0100

    adding cart routes

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,4 +1,4 @@
-import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts, getRecommendedProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getAllProducts, getFeaturedProducts, getRecommendedProducts, toggleFeaturedProduct } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 import express from "express"
 
@@ -8,6 +8,7 @@ router.get('/',protectRoute,adminRoute, getAllProducts)
 router.get('/featured', getFeaturedProducts)
 router.get('/recommendations',protectRoute,getRecommendedProducts)
 router.post("/",protectRoute,adminRoute,createProduct)
+router.patch("/:id",protectRoute,adminRoute,toggleFeaturedProduct)
 router.delete("/:id",protectRoute,adminRoute,deleteProduct)
 
 
