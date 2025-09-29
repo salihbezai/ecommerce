2d86a6de8b1157a3f92f41ca76385f1211c3b961
salihbezai <salihbezai98@gmail.com>
2025-09-29 19:41:54 +0100

    checkout session route

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -12,21 +12,18 @@ router.post('/checkout-success',protectRoute,async (req,res)=>{
     const {sessionId} = req.body
     if(!sessionId) return res.status(400).json({message:"Session ID is required"})
     try {
+        const { sessionId } = req.body
         const session = await stripe.checkout.sessions.retrieve(sessionId)
+
         if(session.payment_status === 'paid'){
-            // mark coupon as used
             if(session.metadata.couponCode){
-                const coupon = await Coupon.findOneAndUpdate({code:session.metadata.couponCode},
-                    {isActive:false},
-                    {new:true}
-                )
-                if(coupon){
-                    coupon.isActive = false
-                    await coupon.save()
-                }
+                await Coupon.findOneAndUpdate({
+                    code: session.metadata.couponCode,
+                    userId: session.metadata.userId
+                })
+                
             }
         }
-        res.json({message:"Success"})
     } catch (error) {
         console.log("Error in checkout-success route",error.message)
         res.status(500).json({ message: "Server error", error: error.message })
